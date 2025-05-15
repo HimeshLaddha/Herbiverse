72f7645e36a628b3943228c059e43bca74763c07
HimeshLaddha <himeshladdha@gmail.com>
2025-05-15 22:29:53 +0530

    Changes for vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,4 +37,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+}
